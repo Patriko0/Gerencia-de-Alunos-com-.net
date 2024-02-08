c90f3b8c09c7ce13af44326b8c7ae171e5685165
Patriko0 <patrikamerico5@gmail.com>
2024-02-08 12:00:00 -0300

    Classe de cursos

--- Gerencia de Alunos/classes/Curso.cs
+++ Gerencia de Alunos/classes/Curso.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gerencia_de_Alunos.classes.interfaces;
 
 namespace Gerencia_de_Alunos.classes
 {
-    internal class Curso
+    public class Curso : Identity
     {
+        public string name { get; set; }
+        public int id { get; }
+        readonly private List<Disciplina> disciplinas = new List<Disciplina>();
+
+        public Curso (string name, int id, Disciplina disciplina)
+        {
+            this.name = name;
+            this.id = id;
+            this.disciplinas.Add(disciplina);
+        }
+
+        public List<string> getDisciplinas()
+        {
+            List<string> disciplinas = new List<string>();
+
+            foreach(Disciplina disc in this.disciplinas) {
+                disciplinas.Add(disc.name);
+            }
+
+            return disciplinas;
+        }
+
+        public void addDisciplina(Disciplina disc)
+        {
+            disciplinas.Add(disc);
+        }
+
+
+        public void removeDisciplina(int id)
+        {
+            List<Disciplina> disc = this.disciplinas.Where(item => item.id == id).ToList();
+            disciplinas.Remove(disc[0]);
+        }
+
+        public void show() => Console.WriteLine("Nome: {0}", name);
     }
 }
