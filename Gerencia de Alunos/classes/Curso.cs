35a958733f5db06334f823a2e2aaeed5fd889c66
Patrik Américo <Patrik Amrico@DESKTOP-SEHC563>
2024-02-08 00:07:22 -0300

    Adicionar arquivos de projeto.

---
+++ Gerencia de Alunos/classes/Curso.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerencia_de_Alunos.classes
+{
+    internal class Curso
+    {
+    }
+}
---
+++ Gerencia de Alunos/classes/interfaces/Screen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerencia_de_Alunos.classes.interfaces
+{
+    abstract class Screen
+    {
+        private bool active = false;
+    }
+}
