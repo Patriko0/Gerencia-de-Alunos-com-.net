4cc665b3e752b0d085d671b787c082d5dafb02f3
Patriko0 <patrikamerico5@gmail.com>
2024-02-08 11:58:40 -0300

    Classe de disciplinas

--- Gerencia de Alunos/classes/Disciplina.cs
+++ Gerencia de Alunos/classes/Disciplina.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gerencia_de_Alunos.classes.interfaces;
 
 namespace Gerencia_de_Alunos.classes
 {
-    internal class Disciplina
+    public class Disciplina : Identity
     {
+        public string name { get; set; }
+
+        public int id { get; }
+
+        public int carg_hr;
+
+        public Disciplina(string name, int id, int carg_hr)
+        {
+            this.name = name;
+            this.id = id;
+            this.carg_hr = carg_hr;
+        }
+
+        public void update(string name, int? carg_hr)
+        {
+            this.name = name;
+            this.carg_hr = carg_hr ?? this.carg_hr;
+        }
+
+        public void show() => Console.WriteLine("Nome: {0}\nCarga horaria: {1}", name, carg_hr);
+        
     }
 }
---
+++ Gerencia de Alunos/classes/interfaces/Identity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerencia_de_Alunos.classes.interfaces
+{
+    public interface Identity
+    {
+        string name { get; set; }
+        int id { get; }
+    }
+}
