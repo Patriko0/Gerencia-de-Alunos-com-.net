61823da4275b32595d970bde7be2dc0c4c970fb0
Patriko0 <patrikamerico5@gmail.com>
2024-02-08 01:33:59 -0300

    Classe para telas de escolhas adicionado

---
+++ Gerencia de Alunos/classes/Screen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerencia_de_Alunos.classes.interfaces
+{
+    abstract class Screen
+    {
+        protected bool active = false;
+        protected Dictionary<int, Action> actions;
+        protected List<string> actionsName;
+
+        protected int qtsActions() { return actions.Count; }
+        protected void addAction(Action newAction, string name) { this.actions.Add(this.qtsActions(), newAction); actionsName.Add(name); }
+
+        public void show()
+        {
+            int option;
+            while (active) {
+                Console.Clear();
+                this.showOptions();
+                Console.WriteLine("Digite uma opção: ");
+
+                option = Console.Read();
+                if(option >= this.qtsActions() + 1) {
+                    active = false;
+                    continue;
+                }
+
+                actions[option]();
+
+            }
+        }
+
+        public void showOptions() {
+            foreach(string name in this.actionsName)
+            {
+                Console.WriteLine(this.actionsName.IndexOf(name).ToString() + ". " + name);
+            }
+            Console.WriteLine((this.qtsActions() + 1).ToString() + ". Sair");
+        }
+
+
+        
+    }
+}
