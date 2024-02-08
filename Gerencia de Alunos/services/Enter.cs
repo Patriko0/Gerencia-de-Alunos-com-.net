6273df00414c9b9dea53201d1cfe1cfaa98b1386
Patriko0 <patrikamerico5@gmail.com>
2024-02-08 02:05:44 -0300

    Enter service

---
+++ Gerencia de Alunos/services/Enter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerencia_de_Alunos.services
+{
+    public class Enter
+    {
+        public static void pressEnter()
+        {
+            Console.Write("\nPressione enter para continuar");
+            Console.ReadLine();
+        }
+    }
+}
